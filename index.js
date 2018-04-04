a1fc7851976765f163eae1bbfe5db40dc8bee779
cwizard2011 <cwizard2011@gmail.com>
2018-04-04 19:28:38 +0100

    client and server and billing routes

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ import './models/User'
 import mongoose from 'mongoose';
 import  './services/passport';
 import keys from './config/keys';
-import authRoutes from './routes/authRoutes'; 
+import authRoutes from './routes/authRoutes';
+import billingRoutes from './routes/billingRoutes'; 
 import cookieSession from 'cookie-session';
 import passport from 'passport';
+import bodyParser from 'body-parser';
+import path from 'path'
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -21,6 +25,15 @@ app.use(passport.initialize());
 app.use(passport.session())
 
 authRoutes(app);
+billingRoutes(app);
 
-const PORT = process.env.PORT || 3000
+if (process.env.NODE_ENV=== 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+const PORT = process.env.PORT || 5000
 app.listen(PORT);
